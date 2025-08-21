78b8e63117ac4825597dd0063122cee6c9fdb682
Vintago <nnrvintagetime9@gmail.com>
2025-08-20 22:55:05 -1100

    /.

--- Library/softwareAdministration/manage.php
+++ Library/softwareAdministration/manage.php
@@ -2,9 +2,9 @@
 require_once '../../processes/database.php';
 $errors = array();
 session_start();
-if (isset($_SESSION['profileTags']) && isset($_SESSION['TmpKeys'])) {
+if (isset($_SESSION['profileTags'])) {
     $aidis = $_SESSION['profileTags'];
-    $TmpKeys = $_SESSION['TmpKeys'];
+    // $TmpKeys = $_SESSION['TmpKeys'];
     $name = $_SESSION['username'];
 } else {
     header ('location: ../../libs.php');
@@ -23,35 +23,19 @@ if (isset($_SESSION['profileTags']) && isset($_SESSION['TmpKeys'])) {
     <link rel="stylesheet" href="../../styling/footer.css">
     <title>Management Dashboard</title>
 </head>
-<body class="w100p flex fld gap10">
-    <section class="posf lt0 pad-s w20 flex fld">
+<body class="h100 bg-2 flex fld gap10">
+    <section class="posf lt0 pad-s w20 h100 bg-4 flex fld">
         <h2 class="txt-n">Manage Software</h2>
     </section>
-    <section class="w75p flex fld gap-s">
-        <div class="sideMg w100p flex"></div>
+    <section class="leftMg pad-n w75p h25 flex fld gap-s">
+        <div class="rightMg pad-s w33p h100p bg-5 flex fld acjc bora-n">
+            <h2 class="txt-b">Add Software/Game</h2>
+        </div>
     </section>
     <section class="leftMg w75p flex gap-s">
         <div class="rightMg w30p flex fld">
-            <img src="" alt="" class="icon-b r16-9">
-            <h2 class="txt-n">Software publication</h2>
-            <div class="sideMg w100p flex">
-                <h2 class="autoMg txt-s">View</h2>
-                <h2 class="autoMg txt-s">Edit</h2>
-                <h2 class="autoMg txt-s">Archive</h2>
-            </div>
-        </div>
-        <div class="rightMg w30p flex fld">
-            <img src="" alt="" class="icon-b r16-9">
-            <h2 class="txt-n">Software publication</h2>
-            <div class="sideMg w100p flex">
-                <h2 class="autoMg txt-s">View</h2>
-                <h2 class="autoMg txt-s">Edit</h2>
-                <h2 class="autoMg txt-s">Archive</h2>
-            </div>
-        </div>
-        <div class="rightMg w30p flex fld">
-            <img src="" alt="" class="icon-b r16-9">
-            <h2 class="txt-n">Software publication</h2>
+            <img src="" alt="" class="w100p r16-9">
+            <h2 class="txt-n">Published Software</h2>
             <div class="sideMg w100p flex">
                 <h2 class="autoMg txt-s">View</h2>
                 <h2 class="autoMg txt-s">Edit</h2>
